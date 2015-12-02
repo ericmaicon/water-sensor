ae275d1a0cee25205d4c8a7935e45a3668743c79
Eric Maicon <eric@ericmaicon.com.br>
2015-12-01 23:15:00 -0200

    changing from express to serial reader

--- js/receive/main.js
+++ js/receive/main.js
@@ -1,37 +1,33 @@
-var express = require('express');
-var sqlite3 = require("sqlite3").verbose();
-
-//express
-var app = express();
-
 //sqlite
+var sqlite3 = require("sqlite3").verbose();
 var dbFile = "../db/sync.db";
 var db = new sqlite3.Database(dbFile);
 
-//Route
-app.get('/', function (req, res) {
-    var params = req.query;
-    var device_id = params.device_id;
-    var flow = params.flow;
-    var time = params.time;
-
-    if (device_id && flow && time) {
-        var stmt = db.prepare("INSERT INTO 'flow' (device_id, flow, time, date) VALUES (?, ?, ?, (DATETIME('now')))");
-        stmt.run({
-            1: device_id,
-            2: flow,
-            3: time
-        });
-        stmt.finalize();
-        console.log("Device: " + device_id + ", Flow: " + flow + ", Time: " + time + " saved.");
-    }
-    res.send("");
+//serial
+var SerialPort = require("serialport").SerialPort;
+var serialPort = new SerialPort("/dev/ttyAMA0", {
+  baudrate: 59600
 });
 
-//Server
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+serialPort.on("open", function () {
+    console.log('open');
+    serialPort.on('data', function(data) {
+        console.log('data received: ' + data);
+        console.log('data received: ' + JSON.parse(data));
+        // var params = req.query;
+        // var device_id = params.device_id;
+        // var flow = params.flow;
+        // var time = params.time;
 
-    console.log('App listening at http://%s:%s', host, port);
+        // if (device_id && flow && time) {
+        //     var stmt = db.prepare("INSERT INTO 'flow' (device_id, flow, time, date) VALUES (?, ?, ?, (DATETIME('now')))");
+        //     stmt.run({
+        //         1: device_id,
+        //         2: flow,
+        //         3: time
+        //     });
+        //     stmt.finalize();
+        //     console.log("Device: " + device_id + ", Flow: " + flow + ", Time: " + time + " saved.");
+        // }
+    });
 });
