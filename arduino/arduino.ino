0a2576c0d929be67d6e8a5cd809fb214ef7194e5
Eric Maicon <eric@ericmaicon.com.br>
2016-02-14 14:43:14 -0200

    arduino code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,56 +1,158 @@
-/*
- * EEPROM Read
- *
- * Reads the value of each byte of the EEPROM and prints it
- * to the computer.
- * This example code is in the public domain.
- */
+#include <SoftwareSerial.h>
 
-#include <EEPROM.h>
+//pins configuration
+byte sensorInterrupt = 0;  // 0 = digital pin 2
+byte sensorPin = 2;
 
-// start reading from the first byte (address 0) of the EEPROM
-int address = 0;
-byte value;
+//Wifi + Server config
+String ssid = "GVT-3EF8";
+String pwd = "0148272560";
+String serverIp = "192.241.153.56";
+int serverPort = 80;
+String deviceId = "1";
 
-void setup() {
-  // initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+//Serial config
+SoftwareSerial wifi(10, 11); // RX, TX
+
+
+//pulse vars
+volatile byte pulseCount;
+float pulse;
+float pulseAmount;
+float oldPulseAmount;
+
+unsigned long oldTime;
+
+/**
+ * Setup
+ **/
+void setup()
+{
+    Serial.begin(9600);
+    wifiStart();
+
+    pinMode(sensorPin, INPUT);
+    digitalWrite(sensorPin, HIGH);
+
+    pulseCount = 0;
+    pulse = 0;
+    pulseAmount = 0;
+    oldTime = 0;
+
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
 }
 
-void loop() {
-  // read a byte from the current address of the EEPROM
-  value = EEPROM.read(address);
+/**
+ * Main program loop
+ **/
+void loop()
+{
+    if ((millis() - oldTime) > 1000)
+    {
+        // Disable the interrupt while calculating flow rate
+        detachInterrupt(sensorInterrupt);
 
-  Serial.print(address);
-  Serial.print("\t");
-  Serial.print(value, DEC);
-  Serial.println();
+        //the amount of pulse is calculated per seconds. If the flow stops in the middle of a second, 
+        //the following code calculate the fraction of it.
+        pulse = ((1000.0 / (millis() - oldTime)) * pulseCount);
 
-  /***
-    Advance to the next address, when at the end restart at the beginning.
+        oldTime = millis();
 
-    Larger AVR processors have larger EEPROM sizes, E.g:
-    - Arduno Duemilanove: 512b EEPROM storage.
-    - Arduino Uno:        1kb EEPROM storage.
-    - Arduino Mega:       4kb EEPROM storage.
 
-    Rather than hard-coding the length, you should use the pre-provided length function.
-    This will make your code portable to all AVR processors.
-  ***/
-  address = address + 1;
-  if (address == EEPROM.length()) {
-    address = 0;
-  }
+        oldPulseAmount = pulseAmount;
 
-  /***
-    As the EEPROM sizes are powers of two, wrapping (preventing overflow) of an
-    EEPROM address is also doable by a bitwise and of the length - 1.
+        //sum all pulses
+        pulseAmount += pulse;
+        if (pulseAmount == oldPulseAmount and pulseAmount != 0) {
+            Serial.println(pulseAmount);
+            sendOrSave(pulseAmount);
+            pulseAmount = 0;
+        }
 
-    ++address &= EEPROM.length() - 1;
-  ***/
+        // Reset the pulse counter so we can start incrementing again
+        pulseCount = 0;
 
-  delay(500);
+        // Enable the interrupt again now that we've finished sending output
+        attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+    }
+}
+
+/**
+ * Connect to the serial
+ * Start the wifi
+ **/
+void wifiStart ()
+{
+    wifi.begin(9600);
+    sendCommand("AT+RST", "OK", 60);
+    //bool connected = wifi.joinAP(ssid, pwd);
+    //wifi.enableMUX();
+}
+
+/**
+ * Send to the API
+ **/
+void sendOrSave (float pulseAmount)
+{
+    String params = "device_id=" + deviceId + "&pulse=" + pulseAmount;
+    String command = "POST /index.php?r=api/create HTTP/1.1\r\n";
+    command += "Host: 192.241.153.56\r\n";
+    command += "Content-Type: application/x-www-form-urlencoded\r\n";
+    command += "Content-Length: " + String(params.length()) + "\r\n\r\n";
+    command += params;
+
+    int commandSize = command.length();
+
+    char charBuf[commandSize];
+    command.toCharArray(charBuf, commandSize);
+
+    //wifi.createTCP(serverIp, serverPort);
+    //10 = counting \r\n for each line on the command above
+    Serial.println(command);
+    //wifi.send(4, (uint8_t *)charBuf, commandSize + 10);
+}
+
+bool sendCommand(String cmd, String goodResponse, unsigned long timeout) {
+    Serial.println("espSendCommand( " + cmd + " , " + goodResponse + " , " + String(timeout) + " )" );
+    wifi.println(cmd);
+    unsigned long tnow = millis();
+    unsigned long tstart = millis();
+    unsigned long execTime = 0;
+    String response = "";
+    char c;
+    while( true ) {
+        if( tnow > tstart + timeout ) {
+            Serial.println("espSendCommand: FAILED - Timeout exceeded " + String(timeout) + " seconds" );
+            if( response.length() > 0 ) {
+                Serial.println("espSendCommand: RESPONSE:");
+                Serial.println( response );
+            } else {
+                Serial.println("espSendCommand: NO RESPONSE");
+            }
+            return false;
+        }
+        c = wifi.read();
+        if( c >= 0 ) {
+            response += String(c);
+            if( response.indexOf(goodResponse) >= 0 ) {
+                execTime = ( millis() - tstart );
+                Serial.println("espSendCommand: SUCCESS - Response time: " + String(execTime) + "ms");
+                Serial.println("espSendCommand: RESPONSE:");
+                Serial.println(response);
+                while(wifi.available() > 0) {
+                    Serial.write(wifi.read());
+                }
+                return true;
+            }
+        }
+        tnow = millis();
+    }
+}
+
+/**
+ * Initerrupt Function
+ **/
+void pulseCounter ()
+{
+    pulseCount++;
 }
