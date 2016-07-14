6acba7e1747cd0147a595c4f0029e005943f8ee4
Eric Maicon <eric@ericmaicon.com.br>
2016-07-13 22:56:39 -0300

    change on the restart logic

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,61 +2,73 @@
 
 byte sensorInterrupt = 1;
 byte sensorPin = 3;
+byte redLedPin = 4;
+byte greenLedPin = 5;
 
 //CHANGE
-String deviceId = "3";
-uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x03,0x03};
-const static uint8_t ip[] = {192,168,25,203};
+String deviceId = "7";
+uint8_t mac[6] = {0x00,0x01,0x02,0x07,0x07,0x07};
+const static uint8_t ip[] = {192,168,25,207};
+
+
+
+
+String token = "270590391d1e9dc96c77ab762d287007";
 //CHANGE
 
 const static uint8_t gw[] = {192,168,25,1};
 const static uint8_t dns[] = {192,168,25,1};
 const char website[] PROGMEM = "ruanydepaula.com.br";
-const static uint8_t serverIp[] = {192,241,153,56};
 byte Ethernet::buffer[700];
 static byte session;
 Stash stash;
 volatile byte pulseCount;  
 float pulse;
-float pulseAmount;
-float oldPulseAmount;
+float pulseTotal;
 unsigned long oldTime;
-long startTime;
+unsigned long lastValidatedTime;
+bool checkReset = false;
 
 void setup()
 {
     Serial.begin(9600);
-    networkStart();
 
     pinMode(sensorPin, INPUT_PULLUP);
+    pinMode(redLedPin, OUTPUT);
+    pinMode(greenLedPin, OUTPUT);
     attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+
+    setLed(redLedPin, HIGH);
+    setLed(greenLedPin, HIGH);
+    delay(2000);
+    setLed(redLedPin, LOW);
+    setLed(greenLedPin, LOW);
+
+    lastValidatedTime = millis();
+    pulseTotal = 0;
+    networkStart();
 }
 
 void loop()
 {
     receiveAnswer();
-    
+    resetIfNecessary();
+
     if ((millis() - oldTime) > 1000)
     { 
         detachInterrupt(sensorInterrupt);
 
         pulse = ((1000.0 / (millis() - oldTime)) * pulseCount);
-        Serial.println("Pulse: " + String(pulse));
-
-        if (pulseAmount == 0 and pulseCount > 0) {
-            startTime = millis();
+        Serial.println("Pulse: " + String(pulse) + ", Available memory: " + String(availableMemory()));
+        
+        if (pulse != 0) {
+            sendToServer(pulse);
+            pulseTotal += pulse;
         }
 
-        oldPulseAmount = pulseAmount;
-        pulseAmount += pulse;
-
-        if (pulseAmount == oldPulseAmount and pulseAmount != 0) {
-            float timeAmount = (millis()-startTime)/1000;
-            Serial.println("Pulse Amount: " + String(pulseAmount));
-            Serial.println("Time Amount: " + String(timeAmount));
-        
-            sendToServer(pulseAmount, timeAmount);
-            pulseAmount = 0;
+        if (pulse == 0 && pulseTotal != 0) {
+            Serial.println("Pulse Amount: " + String(pulseTotal));
+            pulseTotal = 0;
         }
 
         oldTime = millis();
@@ -68,25 +80,33 @@ void loop()
 
 void networkStart ()
 {
+    setLed(redLedPin, HIGH);
     Serial.println("Starting ethernet...");
     if (ether.begin(sizeof Ethernet::buffer, mac, 10) == 0) {
+        checkReset = true;
         Serial.println(F("Failed to access Ethernet controller"));
     }
     
     if (!ether.staticSetup(ip, gw, dns)) {
+        checkReset = true;
         Serial.println(F("Static IP failed"));
     }
 
     ether.printIp("IP:  ", ether.myip);
-
     dnsLookup();
+    setLed(redLedPin, LOW);
+    setLed(greenLedPin, HIGH);
 }
 
-void sendToServer (float pulseAmount, float timeAmount)
+void sendToServer (float pulseAmount)
 {
+    checkReset = true;
+    setLed(redLedPin, HIGH);
+    setLed(greenLedPin, LOW);
+    
     dnsLookup();
 
-    String params = "device_id=" + deviceId + "&pulse=" + String(pulseAmount) + "&time=" +  String(timeAmount);
+    String params = "device_id=" + deviceId + "&pulse=" + String(pulseAmount) + "&time=1&token=" + token;
     Serial.println("Sending: " + params + " Size: " + String(params.length()));
 
     byte sd = stash.create();
@@ -110,6 +130,7 @@ void sendToServer (float pulseAmount, float timeAmount)
 void dnsLookup ()
 {
     if (!ether.dnsLookup(website)) {
+        checkReset = true;
         Serial.println(F("DNS failed"));
     }
 
@@ -122,7 +143,12 @@ void receiveAnswer ()
   
     const char* reply = ether.tcpReply(session);
     if (reply != 0) {
-        Serial.println(reply);
+        //Serial.println(reply);
+        Serial.println("ok");
+        setLed(redLedPin, LOW);
+        setLed(greenLedPin, HIGH);
+        checkReset = false;
+        lastValidatedTime = millis();
     }
 }
 
@@ -131,3 +157,24 @@ void pulseCounter ()
     pulseCount++;
 }
 
+void setLed (byte led, int statusLed)
+{
+    digitalWrite(led, statusLed);
+}
+
+void resetIfNecessary () {
+    //Serial.println("Checking if reset is necessary: " + String(millis() - lastValidatedTime) + ", bool: " + String(checkReset));
+    if (checkReset and millis() - lastValidatedTime > 60000) {
+        asm volatile("  jmp 0");
+    }
+}
+
+int availableMemory() {
+    int size = 1024; // Use 2048 with ATmega328
+    byte *buf;
+
+    while ((buf = (byte *) malloc(--size)) == NULL);
+    free(buf);
+
+    return size;
+}
